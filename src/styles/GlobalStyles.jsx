bcdceff00037e7388974897a78a0d5081cfaa4b9
Allisson100 <allisson.boiane@outlook.com>
2024-03-24 19:51:58 -0300

    feat: createSetUp

---
+++ src/styles/GlobalStyles.jsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        outline: 0;
+        padding: 0;
+    }
+
+    #root {
+        min-height: 100vh;
+        width: 100%;
+    }
+
+    body {
+        background-color: #6734dd;
+    }
+`;
