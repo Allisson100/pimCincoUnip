28da49332c3d62ef78ed9d52466fc6f8d548203e
Allisson100 <allisson.boiane@outlook.com>
2024-03-25 01:44:14 -0300

    feat: createButtonCardComponent

---
+++ src/components/ButtonCard/index.jsx
@@ -0,0 +1,39 @@
+import { Box, Typography, useTheme } from "@mui/material";
+
+const ButtonCard = ({ children, icon }) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      width={200}
+      height={200}
+      borderRadius={2}
+      display="flex"
+      justifyContent="center"
+      alignContent="center"
+      flexDirection="column"
+      sx={{
+        border: "2px solid white",
+        backgroundColor: theme.dark.colors.palette.purple.lavender,
+        transition: "0.2s",
+        cursor: "pointer",
+
+        "&:hover": {
+          backgroundColor: theme.dark.colors.palette.purple.veronica,
+        },
+      }}
+    >
+      <Box textAlign="center">{icon}</Box>
+
+      <Typography
+        textAlign="center"
+        fontSize="1.2rem"
+        color={theme.dark.colors.defaultMain}
+      >
+        {children}
+      </Typography>
+    </Box>
+  );
+};
+
+export default ButtonCard;
