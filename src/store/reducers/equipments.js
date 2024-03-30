53948bb3b51bd85854c6844a8abb2651b69343f6
Allisson100 <allisson.boiane@outlook.com>
2024-03-30 12:13:45 -0300

    fix: createEquipments

--- src/store/reducers/equipments.js
+++ src/store/reducers/equipments.js
@@ -1,18 +1,55 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
+import icons from "../../assets/icons";
 
 const initialState = [
   {
     id: uuidv4(),
-    name: "DataShow",
-    icon: "teste",
-    reserve: ["25/03/2024-13h00", "25/03/2024-13h00", "25/03/2024-13h00"],
+    name: "Projetor",
+    icon: icons[0].icon,
+    reserve: [],
   },
   {
     id: uuidv4(),
-    name: "Tv com vcr",
-    icon: "teste",
-    reserve: ["25/03/2024-13h00", "25/03/2024-13h00", "25/03/2024-13h00"],
+    name: "Tela de Projeção",
+    icon: icons[1].icon,
+    reserve: [],
+  },
+  {
+    id: uuidv4(),
+    name: "Material de Ciência",
+    icon: icons[2].icon,
+    reserve: [],
+  },
+  {
+    id: uuidv4(),
+    name: "DVD",
+    icon: icons[3].icon,
+    reserve: [],
+  },
+  {
+    id: uuidv4(),
+    name: "Notebook",
+    icon: icons[4].icon,
+    reserve: [],
+  },
+  {
+    id: uuidv4(),
+    name: "Microfone",
+    icon: icons[5].icon,
+    reserve: [],
+  },
+  {
+    id: uuidv4(),
+    name: "Caixa de Som",
+    icon: icons[6].icon,
+    reserve: [],
+  },
+  {
+    id: uuidv4(),
+    name: "Televisão",
+    icon: icons[7].icon,
+    reserve: [],
   },
 ];
 
