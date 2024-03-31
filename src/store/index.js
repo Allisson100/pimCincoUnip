726b99b13a9526d6316602233532ee20c53b09e2
Allisson100 <allisson.boiane@outlook.com>
2024-03-31 00:05:25 -0300

    feat: addSerializableCheckConfig

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,10 @@ const store = configureStore({
   reducer: {
     equipments: equipmentsSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
