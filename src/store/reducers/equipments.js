215b9d050bc2e6a4cf6145da11bcc4dd6a42f809
Allisson100 <allisson.boiane@outlook.com>
2024-03-31 00:10:19 -0300

    fix: addCategoryPropertytoState

--- src/store/reducers/equipments.js
+++ src/store/reducers/equipments.js
@@ -7,48 +7,56 @@ const initialState = [
     id: uuidv4(),
     name: "Projetor",
     icon: icons[0].icon,
+    category: "eletrônico",
     reserve: [],
   },
   {
     id: uuidv4(),
     name: "Tela de Projeção",
     icon: icons[1].icon,
+    category: "eletrônico",
     reserve: [],
   },
   {
     id: uuidv4(),
     name: "Material de Ciência",
     icon: icons[2].icon,
+    category: "ciência",
     reserve: [],
   },
   {
     id: uuidv4(),
     name: "DVD",
     icon: icons[3].icon,
+    category: "eletrônico",
     reserve: [],
   },
   {
     id: uuidv4(),
     name: "Notebook",
     icon: icons[4].icon,
+    category: "eletrônico",
     reserve: [],
   },
   {
     id: uuidv4(),
     name: "Microfone",
     icon: icons[5].icon,
+    category: "eletrônico",
     reserve: [],
   },
   {
     id: uuidv4(),
     name: "Caixa de Som",
     icon: icons[6].icon,
+    category: "eletrônico",
     reserve: [],
   },
   {
     id: uuidv4(),
     name: "Televisão",
     icon: icons[7].icon,
+    category: "eletrônico",
     reserve: [],
   },
 ];
@@ -58,7 +66,7 @@ const equipmentsSlice = createSlice({
   initialState: initialState,
   reducers: {
     addEquipment: (state, { payload }) => {
-      state.push(...payload);
+      state.push(payload);
     },
   },
 });
